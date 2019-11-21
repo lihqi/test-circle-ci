cccfda171376ef61075b7523ddb4ec989fb3449a
lihqi <455711093@qq.com>
2019-11-21 14:37:48 +0800

    y

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -1,5 +1,5 @@
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
-
+git config --global user.email "455711093@qq.com"
+git config --global user.name "lihqi"
+echo "machine github.com login lihqi password $GITHUB_TOKEN" > ~/.netrc
 yarn add --dev gh-pages
 yarn run deploy
