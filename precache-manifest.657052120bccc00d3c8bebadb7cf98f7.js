66477307529623c398aa2d6b875374884cff2235
lihqi <455711093@qq.com>
2019-11-21 07:10:02 +0000

    Updates

---
+++ precache-manifest.657052120bccc00d3c8bebadb7cf98f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05e765b359d9a953423deb1c4507e48",
+    "url": "/test-circle-ci/index.html"
+  },
+  {
+    "revision": "24db1db628797c79c8af",
+    "url": "/test-circle-ci/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f90ae9de368a5b4dbe8d",
+    "url": "/test-circle-ci/static/js/2.fd876a04.chunk.js"
+  },
+  {
+    "revision": "24db1db628797c79c8af",
+    "url": "/test-circle-ci/static/js/main.b20edaed.chunk.js"
+  },
+  {
+    "revision": "00e7ebb00f363766b900",
+    "url": "/test-circle-ci/static/js/runtime-main.9d165d48.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-circle-ci/static/media/logo.25bf045c.svg"
+  }
+]);
