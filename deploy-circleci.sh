8eba3c6a9aa1cb634d61b57511cb0397432a0af9
lihqi <455711093@qq.com>
2019-11-21 15:12:45 +0800

    test

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -1,5 +1,5 @@
-git config --global user.email "455711093@qq.com"
-git config --global user.name "lihqi"
+git config --global user.email "45093@qq.com"
+git config --global user.name "li4354353i"
 
 yarn add --dev gh-pages
 yarn run deploy
